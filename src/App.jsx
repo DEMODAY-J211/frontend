eb1594b7bf0419d68bf3b03206498ec0ef3ee925
이예나 <iyena@iyenas-MacBook-Pro.local>
2025-10-16 23:10:21 +0900

    first

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+//여기서부터 자기 이름 밑으로 import 하기!!
+// 예시: import Login from './pages/Login.jsx';
+
+//오서현
+
+//이예나
+
+//주현수
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+       {/* 예시: <Route path='/mypage/festival' element={<MyPage/>}/> */}
+       
+      </Routes>
+    </div>
+  )
+}
+
+export default App
