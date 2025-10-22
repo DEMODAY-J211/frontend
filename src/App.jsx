67e6c9c3b3ba1be05869ce274eeca3ba8c5c7941
hyunneee <sh_oh@u.sogang.ac.kr>
2025-10-22 19:22:39 +0900

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 //여기서부터 자기 이름 밑으로 import 하기!!
 // 예시: import Login from './pages/Login.jsx';
 
 //오서현
+import HomeUser from "../src/pages/user/HomeUser";
 
 //이예나
 
@@ -14,11 +15,11 @@ const App = () => {
   return (
     <div>
       <Routes>
-       {/* 예시: <Route path='/mypage/festival' element={<MyPage/>}/> */}
-       
+        {/* 예시: <Route path='/mypage/festival' element={<MyPage/>}/> */}
+        <Route path="/user" element={<HomeUser />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
