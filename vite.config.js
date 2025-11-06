c9654898e9f5dce84b4b6434a8f4ad68ebcac4ce
주현수 <z_hys@juhyeonsuui-MacBookAir.local>
2025-11-06 22:22:39 +0900

    https/retry/fail

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,17 @@ import react from '@vitejs/plugin-react'
 import React from 'react';
 
 // https://vite.dev/config/
-export default {
-  server: {
-    proxy: {
-      '/auth': {
-        target: 'http://15.164.218.55:8080',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-};
+// export default {
+//   server: {
+//     proxy: {
+//       '/auth': {
+//         target: 'http://15.164.218.55:8080',
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+// };
 
+export default defineConfig({
+  plugins: [react()],});
